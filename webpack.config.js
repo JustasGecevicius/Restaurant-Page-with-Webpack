488b73a1d387c05d0316e933a0e79567ecacb033
Zebaginas <justas.gece@gmail.com>
2022-12-19 22:18:05 +0100

    f

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,6 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    publicPath: "/Restaurant-Page-with-Webpack/",
   },
 };
